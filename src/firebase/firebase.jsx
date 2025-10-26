d9f65cb73a430f06f1f884529787e779b71b3068
ZuriHZ <laxus883@gmail.com>
2025-10-26 14:32:59 -0300

    feat: Actualizar configuración de Firebase para usar variables de entorno

--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -9,21 +9,14 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyD0VHaubqFtv-QucExMyuVjgyMXkAq-oRg",
-
-    authDomain: "backend-01-f0da4.firebaseapp.com",
-
-    databaseURL: "https://backend-01-f0da4-default-rtdb.firebaseio.com",
-
-    projectId: "backend-01-f0da4",
-
-    storageBucket: "backend-01-f0da4.firebasestorage.app",
-
-    messagingSenderId: "844927214053",
-
-    appId: "1:844927214053:web:dc1007fdc36aa328614f0c",
-
-    measurementId: "G-JNNG5HVV83",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
