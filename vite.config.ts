1d36b913160e284c8b27df5228b4bd61fa03df01
ZuriHZ <laxus883@gmail.com>
2026-01-16 17:06:06 -0300

    chore: actualizar configuraciones de proyecto y dependencias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
@@ -8,18 +8,25 @@ export default defineConfig({
         port: 5173,
         open: true,
     },
+    resolve: {
+        dedupe: ["react", "react-dom"],
+    },
     build: {
         rollupOptions: {
             output: {
                 manualChunks: (id) => {
-                    if (id.includes('node_modules')) {
-                        if (id.includes('react') || id.includes('react-dom') || id.includes('react-router')) {
-                            return 'react-vendor';
+                    if (id.includes("node_modules")) {
+                        if (
+                            id.includes("react") ||
+                            id.includes("react-dom") ||
+                            id.includes("react-router")
+                        ) {
+                            return "react-vendor";
                         }
-                        if (id.includes('firebase')) {
-                            return 'firebase';
+                        if (id.includes("firebase")) {
+                            return "firebase";
                         }
-                        return 'vendor';
+                        return "vendor";
                     }
                 },
             },
